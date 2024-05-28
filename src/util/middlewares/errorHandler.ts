8fad38e5dc3630b8eea855e0777e8fbd33c89433
EriksJunior <Eriksjunior13@gmail.com>
2024-05-28 20:33:24 -0300

    refact

--- src/util/middlewares/errorHandler.ts
+++ src/util/middlewares/errorHandler.ts
@@ -1,6 +1,12 @@
 import { NextFunction, Request, Response } from 'express'
 
 function errorHandler (err: any, _: Request, res: Response, next: NextFunction) {
+  if (err.config.url.includes('https://mysql-wpp.jbvwrb.easypanel.host')) {
+    return res.status(err.response.status || 500).json({
+      error: err.response.data.message
+    })
+  }
+
   return res.status(err.statusCode || 500).json({
     error: err.message
   })
